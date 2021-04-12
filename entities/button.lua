e1fb0f313014d2e5f61abfb2ed4a985756576672
Zakk Lundin <zakk.lundin@skola.botkyrka.se>
2021-04-12 19:22:10 +0200

    aligned button-string to the center of all buttons

--- entities/button.lua
+++ entities/button.lua
@@ -10,7 +10,7 @@ return function (x, y, str)
         love.graphics.setColor(0, 30, 15)
         love.graphics.rectangle("fill", x, y, 200, 100)
         love.graphics.setColor(255,255,255)
-        love.graphics.print(str, x, y, 0, 1.5, 1.5)
+        love.graphics.printf(str, x, y, 130, "center", 0, 1.5, 1.5)
     end
     return button
 end
